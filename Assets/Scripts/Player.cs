1b352b4d085a736f212f724f8ed29b8b90db43ce
Stefano Fiumara <sdf10517@gmail.com>
2013-10-17 17:26:58 -0500

    TubeManager and Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public static float distanceTraveled;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		distanceTraveled = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0f, 0f, speed * Time.deltaTime);
+		distanceTraveled = transform.localPosition.z;
+	}
+}
