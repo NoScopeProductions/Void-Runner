cfdb610c6b6a126598deba1fd1e74d20f1a75ba2
xCmac <dsemac@gmail.com>
2013-10-20 23:31:54 -0500

    Random Rotations on Debris

--- Assets/Scripts/DebrisSpawner.cs
+++ Assets/Scripts/DebrisSpawner.cs
@@ -10,7 +10,7 @@ public class DebrisSpawner : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		InvokeRepeating("SpawnDebris", 1.0f, 1.5f);
+		InvokeRepeating("SpawnDebris", 1.0f, 1.0f);
 	}
 	
 	// Update is called once per frame
@@ -23,8 +23,8 @@ public class DebrisSpawner : MonoBehaviour
 	{
 		
 		Vector3 newPosition = Camera.main.transform.position + Camera.main.transform.forward * distance;
-		newPosition.x = Random.Range(-5, 5);
-		newPosition.y = Random.Range(-5, 5);
+		newPosition.x = Random.Range(-10, 10);
+		newPosition.y = Random.Range(-10, 10);
 		Transform debris = (Transform)Instantiate(prefabs[Random.Range(0,5)], newPosition, transform.rotation);
 		//debris.transform.position = Vector3(Random.Range(-20,20) , Random.Range(-20, 20), 0);
 		debris.parent = transform;
