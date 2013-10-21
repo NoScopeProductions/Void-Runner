83db571ed6ca186b6e7e6f25010d6f485ad21264
xCmac <dsemac@gmail.com>
2013-10-20 20:21:03 -0500

    .

--- Assets/Scripts/MenuScripts/CameraMove.cs
+++ Assets/Scripts/MenuScripts/CameraMove.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class CameraMove : MonoBehaviour 
 {
+	public static float distanceTraveled;
 	public float speed;
 	// Use this for initialization
 	void Start () 
 	{
-	
+		distanceTraveled = 0;
 	}
 	
 	// Update is called once per frame
@@ -15,5 +16,6 @@ public class CameraMove : MonoBehaviour
 	{
 		transform.Translate(0f, 0f, speed * Time.deltaTime);
 		transform.RotateAround(Vector3.zero, transform.forward, speed * Time.deltaTime);
+		distanceTraveled = transform.localPosition.z;
 	}
 }
