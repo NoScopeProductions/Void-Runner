738939c628ff13cf331846873a585a60c31d21f4
xCmac <dsemac@gmail.com>
2013-10-20 20:38:50 -0500

    Added Rocks, Debris Manager

---
+++ Assets/Scripts/DebrisSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebrisSpawner : MonoBehaviour 
+{
+	public Transform[] prefabs;
+	public int numberOfObjects;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
