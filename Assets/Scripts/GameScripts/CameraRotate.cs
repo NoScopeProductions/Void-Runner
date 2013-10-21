442106579c5c4c5537f20c3a0156ca7c349a7e6f
Stefano Fiumara <sdf10517@gmail.com>
2013-10-21 18:47:13 -0500

    Camera Rotation Test

---
+++ Assets/Scripts/GameScripts/CameraRotate.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour {
+	
+	public enum RotationState {BOTTOM, TOP, LEFT, RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT, TOP_LEFT, TOP_RIGHT};
+	
+	public static RotationState rotation;
+	
+	
+	// Use this for initialization
+	void Start () {
+		rotation = RotationState.BOTTOM;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(valueBetween(Player.playerPos.x, -35f, -14f) && Player.playerPos.y <= -14f) {
+			if(rotation != RotationState.BOTTOM_LEFT) {
+				rotation = RotationState.BOTTOM_LEFT;
+				iTween.RotateTo(gameObject, iTween.Hash("z", -45, "time", 0.7f));
+			}
+		}
+		else if(valueBetween(Player.playerPos.x, 14f, 34.5f) && Player.playerPos.y <= -14f) {
+			if(rotation != RotationState.BOTTOM_RIGHT) {
+				rotation = RotationState.BOTTOM_RIGHT;
+				iTween.RotateTo(gameObject, iTween.Hash("z", 45, "time", 0.7f));
+			}
+		}
+		else if(valueBetween(Player.playerPos.x, -14f, 14f) && Player.playerPos.y <= -34f) {
+			if(rotation != RotationState.BOTTOM) {
+				rotation = RotationState.BOTTOM;
+				iTween.RotateTo(gameObject, iTween.Hash("z", 0, "time", 0.7f));
+			}
+		}
+		
+		
+	}
+	
+	//for making the update checks a little easier
+	private bool valueBetween(float x, float min, float max) {
+		return (x > min && x < max);
+	}
+}
--- Assets/Scripts/GameScripts/Player.cs
+++ Assets/Scripts/GameScripts/Player.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class Player : MonoBehaviour {
 	
 	public static float distanceTraveled;
+	public static Vector2 playerPos;
 	public float speed;
 	public float turnSpeed;
 	
 	// Use this for initialization
 	void Start () {
 		distanceTraveled = 0;
+		playerPos = new Vector2(0f, 0f);
 	}
 	
 	// Update is called once per frame
@@ -17,14 +19,47 @@ public class Player : MonoBehaviour {
 		transform.Translate(0f, 0f, speed * Time.deltaTime);
 		distanceTraveled = transform.localPosition.z;
 		checkInput();
+		playerPos.x = transform.localPosition.x;
+		playerPos.y = transform.localPosition.y;
+		
 	}
 	
 	void checkInput() {
 		if(Input.GetKey(KeyCode.LeftArrow)) {
-			transform.RotateAround(Vector3.zero, transform.forward, Time.deltaTime * -turnSpeed);	
+			//transform.RotateAround(Vector3.zero, transform.forward, Time.deltaTime * -turnSpeed);
+			MoveLeft();
 		} else if(Input.GetKey(KeyCode.RightArrow)) {
-			transform.RotateAround(Vector3.zero, transform.forward, Time.deltaTime * turnSpeed);	
+			//transform.RotateAround(Vector3.zero, transform.forward, Time.deltaTime * turnSpeed);	
+			MoveRight ();
 		}
+	}
+	
+	void MoveLeft() {
+		switch(CameraRotate.rotation) {
+			case CameraRotate.RotationState.BOTTOM:	
+				transform.Translate(-turnSpeed * Time.deltaTime, 0f, 0f);
+				break;
+			case CameraRotate.RotationState.BOTTOM_LEFT:
+				transform.Translate(-turnSpeed * Time.deltaTime, turnSpeed * Time.deltaTime, 0f);
+				break;
+			case CameraRotate.RotationState.BOTTOM_RIGHT:
+				transform.Translate(-turnSpeed * Time.deltaTime, -turnSpeed * Time.deltaTime, 0f);
+				break;
+		}
+	}
+	
+	void MoveRight() {
+		switch(CameraRotate.rotation) {
+			case CameraRotate.RotationState.BOTTOM:	
+				transform.Translate(turnSpeed * Time.deltaTime, 0f, 0f);
+				break;
+			case CameraRotate.RotationState.BOTTOM_LEFT:
+				transform.Translate(turnSpeed * Time.deltaTime, -turnSpeed * Time.deltaTime, 0f);
+				break;
+			case CameraRotate.RotationState.BOTTOM_RIGHT:
+				transform.Translate(turnSpeed * Time.deltaTime, turnSpeed * Time.deltaTime, 0f);
+				break;
+		}	
 	}
 }
 
