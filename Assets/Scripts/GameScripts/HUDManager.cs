24fc989f2fed308a4c878dbf114413293dd13c3f
xCmac <dsemac@gmail.com>
2013-12-19 23:42:05 -0600

    Added Fuel bar
    New manager (HUDManager) put the fuel bar under the hud manager, right
    now its just dark background but i'll fix that later.
    
    Need to remove FUEL and Score TEXTs and put them in under HUDManager!
    
    (ok im tired off to bed...)

---
+++ Assets/Scripts/GameScripts/HUDManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDManager : MonoBehaviour 
+{
+	private int maxFuel = 100;
+	public float currentFuel = 100;
+	private float fuelBarLength;
+
+	// Use this for initialization
+	void Start () 
+	{
+		fuelBarLength = Screen.width/2;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		currentFuel = Player.instance.fuel;
+	}
+	
+	void OnGUI()
+	{
+		GUI.Box(new Rect(Screen.width / 2, 20, fuelBarLength, 20), Mathf.Round(currentFuel).ToString() + "/" + maxFuel);
+		GUI.backgroundColor = Color.yellow;
+		fuelBarLength = (Screen.width / 2) * (currentFuel / (float)maxFuel);
+	}
+	
+	public void AdjustCurrentFuel(float adjustment)
+	{
+		Player.instance.fuel += adjustment;
+		fuelBarLength = (Screen.width / 2) * (currentFuel / (float)maxFuel);
+	}
+}
