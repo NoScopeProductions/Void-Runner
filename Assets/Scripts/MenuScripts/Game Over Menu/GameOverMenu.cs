ef5002cc5f89e292529ac8663e50f34a42bae608
xCmac <dsemac@gmail.com>
2013-12-23 00:40:44 -0600

    death menu
    added buttons (non functional)
    -Play again
    -Main menu

--- Assets/Scripts/MenuScripts/Game Over Menu/GameOverMenu.cs
+++ Assets/Scripts/MenuScripts/Game Over Menu/GameOverMenu.cs
@@ -19,14 +19,16 @@ public class GameOverMenu : MonoBehaviour
 	void OnGUI () 
 	{
 		// Make a group on the center of the screen
-		GUI.BeginGroup (new Rect (Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 100));
+		GUI.BeginGroup (new Rect (Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 200));
 		// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
 
-		// We'll make a box so you can see where the group is on-screen.
-		GUI.Box (new Rect (0,0,100,100), "Group is here");
+		// make a box so you can see where the group is on-screen.
+		GUI.Box (new Rect (0,0,100,200), "Group is here");
 		GUI.Label (new Rect (10,40,80,30), "Score:" + Player.instance.score.ToString());
 		GUI.Label (new Rect (10,60,80,30), "Score:" + Player.instance.score.ToString());
 		GUI.Label (new Rect (10,80,80,30), "Score:" + Player.instance.score.ToString());
+		GUI.Button (new Rect (10,110,80,30), "Play Again");
+		GUI.Button (new Rect (10,150,80,30), "Main Menu");
 
 		// End the group we started above. This is very important to remember!
 		GUI.EndGroup ();
