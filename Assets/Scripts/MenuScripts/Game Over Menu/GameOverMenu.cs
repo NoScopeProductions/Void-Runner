4413b7369572a4743e2a9652086126f8563ed6ae
xCmac <dsemac@gmail.com>
2013-12-23 00:35:17 -0600

    Added death menu
    still needs work, I left it disabled for now as it doesn't show much.
    
    Need to check for player state dead then enable the manager.

---
+++ Assets/Scripts/MenuScripts/Game Over Menu/GameOverMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverMenu : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	void OnGUI () 
+	{
+		// Make a group on the center of the screen
+		GUI.BeginGroup (new Rect (Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 100));
+		// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
+
+		// We'll make a box so you can see where the group is on-screen.
+		GUI.Box (new Rect (0,0,100,100), "Group is here");
+		GUI.Label (new Rect (10,40,80,30), "Score:" + Player.instance.score.ToString());
+		GUI.Label (new Rect (10,60,80,30), "Score:" + Player.instance.score.ToString());
+		GUI.Label (new Rect (10,80,80,30), "Score:" + Player.instance.score.ToString());
+
+		// End the group we started above. This is very important to remember!
+		GUI.EndGroup ();
+	}
+}
