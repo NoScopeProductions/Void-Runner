591a0c995bc7a1dd5f68824d3dfe42c6835d2dc1
Stefano Fiumara <sdf10517@gmail.com>
2014-01-09 15:28:24 -0600

    Implemented new splash screen

--- Assets/Scripts/Splash/SplashManager.cs
+++ Assets/Scripts/Splash/SplashManager.cs
@@ -3,36 +3,15 @@ using System.Collections;
 
 public class SplashManager : MonoBehaviour 
 {
-	public float wait = 1.0f;
-	public GameObject no, scope, productions;
 
-	// Use this for initialization
-	void Start () 
-	{
-		StartCoroutine("DisplaySplash");
+
+	void Start() {
+		iTween.FadeTo(gameObject, 0f, 1.5f);
+		iTween.FadeTo(gameObject, iTween.Hash("alpha", 1f, "time", 1.5f, "delay", 3f));
+		Invoke("loadMenu", 5f);
 	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	}
-	
-	
-	IEnumerator DisplaySplash()
-	{
-		yield return new WaitForSeconds(wait);
-		no.SetActive(true);
 
-		
-		yield return new WaitForSeconds(0.5f);
-		scope.SetActive(true);
-
-		
-		yield return new WaitForSeconds(0.5f);
-		productions.SetActive(true);
-		
-		yield return new WaitForSeconds(2.0f);
+	void loadMenu() {
 		Application.LoadLevel("Menu");
-
 	}
 }
