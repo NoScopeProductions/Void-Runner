e31a6dde770fe7303d42819f74954ae256860136
xCmac <dsemac@gmail.com>
2014-02-07 22:58:14 -0600

    New Main Menu
    Added new main menu buttons + Removed old unused scrips

---
+++ Assets/Scripts/MenuScripts/Main Menu/Button_Play_Script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button_Play_Script : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnMouseUp()
+	{
+		Application.LoadLevel("Game");
+		Debug.Log("Clicked Play!");
+	}
+}
---
+++ Assets/Scripts/MenuScripts/Main Menu/Button_Settings_Script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button_Settings_Script : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnMouseUp()
+	{
+		Debug.Log("Clicked Settings!");
+	}
+}
