3ddf11dc7a086043d08aff71efc5f51cf7ecc37c
Stefano Fiumara <sdf10517@gmail.com>
2014-07-16 02:04:04 -0500

    Added Fade From Black to main menu

---
+++ Assets/CameraFade.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraFade : MonoBehaviour {
+
+    private float AlphaFadeValue = 1f;
+    public Texture BlackTexture;
+
+    public void OnGUI()
+    {
+        AlphaFadeValue -= Mathf.Clamp01(Time.deltaTime / 5);
+
+        GUI.color = new Color(0, 0, 0, AlphaFadeValue);
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), BlackTexture);
+    }
+}
