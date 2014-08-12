664f42c8f1d1a2b145ffdb4decdd415068199a96
Stefano Fiumara <sdf10517@gmail.com>
2014-08-12 18:37:49 -0500

    Updated credits menu

--- Assets/Scripts/Menu/CreditsMenu.cs
+++ Assets/Scripts/Menu/CreditsMenu.cs
@@ -9,13 +9,13 @@ public class CreditsMenu : MonoBehaviour
     public GameObject CreditsPlane;
 
     private Vector3 DefaultPosition = new Vector3(0f, -478f, 185f);
-    private Vector3 EndPosition = new Vector3(0f, 440f, 185f);
+    private Vector3 EndPosition = new Vector3(0f, 1000f, 185f);
 
 	void OnEnable () 
 	{
 		Vector3[] TweenPath = new Vector3[] {DefaultPosition, EndPosition};
         asteroidManager.IsActive = false;
-		iTween.MoveTo(CreditsPlane, iTween.Hash("path", TweenPath, "easetype", iTween.EaseType.easeInOutSine, "time", 25f));
+		iTween.MoveTo(CreditsPlane, iTween.Hash("path", TweenPath, "easetype", iTween.EaseType.easeInOutSine, "time", 40f));
 
 	}
 
