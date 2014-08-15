4e9769099c29066f19e47b0f71aaa14a5ad3224c
Stefano Fiumara <sdf10517@gmail.com>
2014-08-14 19:59:09 -0500

    Start Game Button goes to Game Scene

---
+++ Assets/Scripts/Menu/Main Menu/StartGameButtonEvents.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StartGameButtonEvents : MonoBehaviour 
+{
+    public CameraFade cameraFade;
+    public dfCoverflow selectionMenu;
+
+	public void OnClick( dfControl control, dfMouseEventArgs mouseEvent )
+	{
+        //GlobalPreferences.shipSelected = WhichShip;
+        switch (selectionMenu.selectedIndex)
+        {
+            case 0:
+                GlobalPreferences.shipSelected = GlobalPreferences.SHIP.REMAKER;
+                break;
+            case 1:
+                GlobalPreferences.shipSelected = GlobalPreferences.SHIP.DEFAULT;
+                break;
+            case 2:
+                GlobalPreferences.shipSelected = GlobalPreferences.SHIP.DSK;
+                break;
+        }
+
+        cameraFade.FadeOut();
+        Invoke("LoadGame", 1.5f);
+	}
+
+    private void LoadGame()
+    {
+        Application.LoadLevel("Game");
+    }
+
+}
