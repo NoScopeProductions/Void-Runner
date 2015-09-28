5bca42203cb428b286257324f3658647b5fffb59
Stefano Fiumara <sdf10517@gmail.com>
2015-09-27 23:56:09 -0500

    Remember last high score name

---
+++ Assets/Scripts/SetLastHighScoreName.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SetLastHighScoreName : MonoBehaviour
+{
+    public void SetHighScoreName()
+    {
+        var text = GetComponent<dfTextbox>();
+
+        text.Text = GlobalPreferences.LastHighScoreName;
+    }
+}
