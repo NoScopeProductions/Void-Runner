d1bad91146ed6f41fa0ba134b159ab57e040e93c
Stefano Fiumara <sdf10517@gmail.com>
2015-09-27 23:55:56 -0500

    Fix inconsistent tutorial checkbox

--- Assets/Scripts/Menu/SkipTutorial.cs
+++ Assets/Scripts/Menu/SkipTutorial.cs
@@ -5,6 +5,13 @@ using System.Collections.Generic;
 public class SkipTutorial : MonoBehaviour 
 {
 
+    public void OnEnable()
+    {
+        var checkbox = GetComponent<dfCheckbox>();
+
+        checkbox.IsChecked = GlobalPreferences.SkipTutorial;
+    }
+
 	public void OnClick( dfControl control, dfMouseEventArgs mouseEvent )
 	{
         GlobalPreferences.SkipTutorial = !GlobalPreferences.SkipTutorial;
